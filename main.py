68de4b6439ee3d7bcdc5dcd779b6e9ee173b9ab5
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2022-02-25 11:40:55 +0100

    ♻️ refactoring

--- main.py
+++ main.py
@@ -1,117 +1,91 @@
+# Python Standard Library
 import time
-from numpy import *
-from scipy.integrate import solve_ivp
+
+# Third-Party Libraries
+import numpy as np
+import scipy.integrate as sci
 import matplotlib.pyplot as plt
-import matplotlib.animation
+import matplotlib.animation as ani
 
 
-# def fun(t, xy):
-#     x, y = xy
-#     return array([-y - 0.5 * x, +x - 0.1 * y])
-
+# Parameters
+# ------------------------------------------------------------------------------
 def fun(t, xy):
     x, y = xy
-    r = sqrt(x*x + y*y)
+    r = np.sqrt(x * x + y * y)
     dx = x + x * y - (x + y) * r
     dy = y - x * x + (x - y) * r
     return [dx, dy]
+
 
 t_span = (0.0, 10.0)
 y0 = [1.0, 0.0]
 
 df = 60.0
 dt = 1.0 / df
+t = np.arange(t_span[0], t_span[1], dt)
+t = np.r_[t, t_span[1]]
 
-r = solve_ivp(fun, t_span, y0, dense_output=True)
-t = linspace(t_span[0], t_span[1], 1000)
-td = arange(t_span[0], t_span[1], dt)
-x_t, y_t = r.sol(t)
-x_td, y_td = r.sol(td)
+radius = 0.5
+n = 10000
+xc, yc = y0
+y0s = np.array(
+    [
+        [xc + radius * np.cos(theta), yc + radius * np.sin(theta)]
+        for theta in np.linspace(0, 2 * np.pi, n)
+    ]
+)
 
-if False:
-    plt.figure()
-    plt.axis([-1.2, 1.2, -1.2, 1.2])
-    plt.axis("equal")
-    plt.plot(x_t, y_t, "--", color="grey")
-    plt.plot(x_td, y_td, "k.")
-    plt.grid(True)
-    plt.show()
 
-if True:
-
-    radius = 0.5
-    n = 10000
-    xc, yc = y0
-    y0s = array([
-        [xc + radius * cos(theta), yc + radius * sin(theta)]
-        for theta in linspace(0, 2 * pi, n)
-    ])
-
-    t_ = time.time()
-    print(0.0)
-
-    rs = []
+def solve(**kwargs):
+    kwargs = kwargs.copy()
+    kwargs["dense_outputs"] = True
+    results = []
     for y0 in y0s:
-        r = solve_ivp(fun, t_span, y0, dense_output=True, rtol=1e-9, atol=1e-100)
-        rs.append(r)
+        kwargs["y0"] = y0
+        result = sci.solve_ivp(**kwargs)
+        results.append(result)
+    return results
 
-    print(time.time() - t_)
+def get_data(results, t):
+    n = len(results)
+    data = np.zeros((len(t), 2, n))
+    for i, r in enumerate(results):
+        sol_t = r.sol(t)
+        data[:, :, i] = sol_t.T
+        return data
 
-    if False:
-        plt.figure()
-        plt.axis([-1.2, 1.2, -1.2, 1.2])
-        plt.axis("equal")
-        plt.plot(x_t, y_t, "--", color="grey")
+def generate_movie(data, filename="movie.mp4"):
+    fig = plt.figure(figsize=(16, 9))
+    axes = fig.subplots()
+    axes.axis("equal")
+    ratio = 16 / 9
+    ym = 1.2
+    xm = ym * ratio
+    print([-xm, xm, -ym, ym])
+    axes.axis([-xm, xm, -ym, ym])
+    fig.subplots_adjust(0, 0, 1, 1)
+    # axes.axis('off')
 
-    data = zeros((len(td), 2, n))
-    for i, r in enumerate(rs):
-        sol_td = r.sol(td)
-        data[:, :, i] = sol_td.T
+    polygon = None
 
-    # for x, y in data:
-    #     plt.fill(x, y, color="k", alpha=0.1)
-    # plt.grid(True)
-    # print(time.time() - t_)
-    # plt.show()
+    def update(i):
+        nonlocal polygon
+        x, y = data[i]
+        if polygon:
+            polygon.remove()
+        polygon = plt.fill(x, y, color="k")[0]
 
-    # x_td = []
-    # y_td = []
-    # for td_ in td:
-    #     x_td.append([])
-    #     y_td.append([])
-    #     for r in rs:
-    #         x_td[-1].append(r.sol(td_)[0])
-    #         y_td[-1].append(r.sol(td_)[1])
+    writer = ani.FFMpegWriter(fps=df)
+    animation = ani.FuncAnimation(fig, func=update, frames=len(data))
+    animation.save("filename", writer=writer, dpi=100)
 
-    # for x, y in zip(x_td, y_td):
-    #     plt.fill(x, y, color="k")
-    # plt.grid(True)
-    # print(time.time() - t_)
-    # plt.show()
-
-fig = plt.figure(figsize=(16, 9))
-axes = fig.subplots()
-axes.axis("equal")
-ratio = 16/9
-ym = 1.2
-xm = ym * ratio
-print([-xm, xm, -ym, ym])
-axes.axis([-xm, xm, -ym, ym])
-print(axes.axis())
-fig.subplots_adjust(0, 0, 1, 1)
-#axes.axis('off')
-
-polygon = None
-
-def func(i):
-    global polygon
-    x, y = data[i]
-    if polygon:
-        polygon.remove()
-    polygon = plt.fill(x, y, color="k")[0]
-
-writer = matplotlib.animation.FFMpegWriter(fps=df)
-anim = matplotlib.animation.FuncAnimation(fig, func, len(td))
-anim.save("movie.mp4", writer=writer, dpi=100)
-
-#plt.show()
+results = solve(
+    fun=fun, 
+    t_span=t_span, 
+    y0s=y0s, 
+    rtol=1e-9, 
+    atol=0,
+)
+data = get_data(results, t)
+generate_movie(data)
